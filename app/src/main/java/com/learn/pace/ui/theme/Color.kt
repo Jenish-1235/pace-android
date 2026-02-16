51041ec79e9609dc289f7126826bed304bf69c4f
Jenish-1235 <jenish.togadiya@triotech.co.in>
2026-02-17 00:37:18 +0530

    update color scheme and theme

--- app/src/main/java/com/learn/pace/ui/theme/Color.kt
+++ app/src/main/java/com/learn/pace/ui/theme/Color.kt
@@ -2,10 +2,8 @@ package com.learn.pace.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val ReadBlack = Color(0xFF111111)
+val ReadWhite = Color(0xFFFAFAFA)
+val MatteBlack = Color(0xFF1E1E20)
+val ErrorRed = Color(0xFFE53935)
+val SuccessGreen = Color(0xFF2E7D32)
