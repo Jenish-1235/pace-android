9a9043b0d476e0a5d257fdbf2a0431caab683543
Jenish-1235 <jenish.togadiya@triotech.co.in>
2026-02-17 00:57:07 +0530

    update color scheme and theme to add success green color

--- app/src/main/java/com/learn/pace/ui/theme/Theme.kt
+++ app/src/main/java/com/learn/pace/ui/theme/Theme.kt
@@ -1,14 +1,15 @@
 package com.learn.pace.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.ColorScheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
@@ -38,10 +39,13 @@ private val LightColorScheme = lightColorScheme(
     onError = ReadWhite,
 )
 
+val ColorScheme.success : Color
+    get() = SuccessGreen
+
 @Composable
 fun PaceTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    dynamicColor: Boolean = true,
+    dynamicColor: Boolean = false,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
@@ -59,4 +63,5 @@ fun PaceTheme(
         typography = Typography,
         content = content
     )
+
 }
